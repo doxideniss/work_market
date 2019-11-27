ff0a81da2b2e52222127926c57d3a67b472dcaf9
Deniss <doxideniss@gmail.com>
2019-11-27 21:57:48 +0200

    products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-    
+
+	var mixer = mixitup('.products__inner-box', {
+		selectors: {
+			target: '.products__item'
+		},
+		animation: {
+			duration: 300
+		}
+	});
+
 })
--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,8 @@ gulp.task('script', () => {
 gulp.task('js', () => {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
