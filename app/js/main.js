1d94fbff83fce6c4605877cad78724d665a89aac
Deniss <doxideniss@gmail.com>
2019-12-08 14:05:30 +0200

    added new-items + icons

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 $(function () {
 
+	$('.icon-th-list').on('click', function () {
+		$('.products__item').addClass('list')
+		$('.icon-th-list').addClass('btn--active')
+		$('.icon-th-large').removeClass('btn--active')
+	})
+
+	$('.icon-th-large').on('click', function () {
+		$('.products__item').removeClass('list')
+		$('.icon-th-large').addClass('btn--active')
+		$('.icon-th-list').removeClass('btn--active')	
+	})
+
+	$(".rate-star").rateYo({
+		fullStar: true,
+		starWidth: "12px",
+		ratedFill: "#ffa726",
+		spacing: "2px",
+		readOnly: true
+	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
 	var mixer = mixitup('.products__inner-box', {
 		selectors: {
 			target: '.products__item'
@@ -9,14 +38,6 @@ $(function () {
 		}
 	});
 
-	$(".rate-star").rateYo({
-		fullStar: true,
-		starWidth: "12px",
-		ratedFill: "#ffa726",
-		spacing: "2px",
-		readOnly: true
-	});
-	
 	$('.product-slider__inner').slick({
 		slidesToScroll: 4,
 		slidesToShow: 4,
