c293fa02c696377f0b7b2287ef8fb6fe7cf144ef
Deniss <doxideniss@gmail.com>
2019-12-10 23:04:29 +0200

    add drop-downs

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('css', function () {
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('_libs.scss'))
@@ -49,6 +50,7 @@ gulp.task('js', () => {
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/mixitup/dist/mixitup.js'
     ])
         .pipe(concat('libs.min.js'))
