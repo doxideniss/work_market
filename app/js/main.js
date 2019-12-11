5c7272cf8d3cdc5d211c2fac6463d61d71e99f45
Deniss <doxideniss@gmail.com>
2019-12-11 21:51:39 +0200

    add responsive header & footer

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,14 @@ $(function () {
 		$('.icon-th-list').removeClass('btn--active')	
 	})
 
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle()
+	})
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active')
+	})
+
 	$(".rate-star").rateYo({
 		fullStar: true,
 		starWidth: "12px",
